a0a3c74eb5be0c0388e4fa284db7cfe0ce6b31c1
Xiaohui Li <xiaohui_li@foxmail.com>
2024-11-30 22:57:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
+let proxyIP = 'proxyip.fxxk.dedyn.io';
 let sub = '';
 let subconverter = 'SUBAPI.fxxk.dedyn.io';
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
